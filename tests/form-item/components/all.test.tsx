6ba8cf052a9c147fb900a5d1f14283e3d1b0e261
zhangchaojie <1098626505@qq.com>
2021-06-05 18:23:31 +0800

    test: form-item components mock 测试

--- tests/form-item/components/all.test.tsx
+++ tests/form-item/components/all.test.tsx
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
-import { message } from 'antd';
+import { message, Form } from 'antd';
 import axios from 'axios';
+import mockjs from 'mockjs';
 import Mock from 'mockjs';
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
 
 import {
   SuperCaptcha,
@@ -204,18 +205,200 @@ const App = () => {
   );
 };
 
-test('all form-item components', () => {
+
+describe('all form-item components', () => {
   const originError = console.error
-  console.error = () => { }
-  const warpper = render(<App />)
-  console.error = originError
-  expect(warpper.container).toMatchSnapshot()
-  userEvent.click(warpper.getByRole('button', { name: /autoPlaceholder/ }))
-  expect(warpper.container).toMatchSnapshot()
-  userEvent.click(warpper.getByRole('button', { name: /hideLabel/ }))
-  expect(warpper.container).toMatchSnapshot()
-  userEvent.click(warpper.getByRole('button', { name: /disabled/ }))
-  expect(warpper.container).toMatchSnapshot()
-  userEvent.click(warpper.getByRole('button', { name: /readonly/ }))
-  expect(warpper.container).toMatchSnapshot()
-})
+  beforeAll(() => {
+    console.error = () => { }
+  })
+  afterAll(() => {
+    console.error = originError
+  })
+  test('show all', () => {
+    const warpper = render(<App />)
+    expect(warpper.container).toMatchSnapshot()
+    userEvent.click(warpper.getByRole('button', { name: /autoPlaceholder/ }))
+    expect(warpper.container).toMatchSnapshot()
+    userEvent.click(warpper.getByRole('button', { name: /hideLabel/ }))
+    expect(warpper.container).toMatchSnapshot()
+    userEvent.click(warpper.getByRole('button', { name: /disabled/ }))
+    expect(warpper.container).toMatchSnapshot()
+    userEvent.click(warpper.getByRole('button', { name: /readonly/ }))
+    expect(warpper.container).toMatchSnapshot()
+  })
+
+  describe('default mock', () => {
+    const App: FC<{}> = ({ children }) => {
+      return <SuperProvider axios={axios} mockjs={mockjs}>
+        <SuperForm mock isResponsive={false}>
+          {children}
+          <Form.Item noStyle shouldUpdate>
+            {(form) => (
+              <div data-testid="data">{JSON.stringify(form.getFieldsValue())}</div>
+            )}
+          </Form.Item>
+        </SuperForm>
+      </SuperProvider>
+    }
+
+    const setUp = (Component: any, name: string, props: any = {}) => {
+      const res = render(<App>
+        <Component name={name} {...props}></Component>
+      </App>)
+
+      userEvent.click(screen.getByRole('button', { name: /Mock 数据/ }))
+      return res
+    }
+
+    const getValue = (name: string) => {
+      const text = screen.getByTestId('data').textContent
+      const data = JSON.parse(text || '{}')
+      return data[name]
+    }
+
+    const toBe = (name: string, value: any) => {
+      expect(getValue(name)).toEqual(expect.any(value))
+    }
+
+    const check = (Component: any, name: string, value: any, props?: any) => {
+      setUp(Component, name, props)
+      toBe(name, value)
+    }
+
+    test('Checkbox is boolean', () => {
+      check(SuperCheckbox, 'checkbox', Boolean)
+    })
+
+    test('CheckboxGroup is Array', () => {
+      const arr = ['a', 'b']
+      setUp(SuperCheckboxGroup, 'checkboxgroup', { options: arr })
+      expect(arr).toEqual(expect.arrayContaining(getValue('checkboxgroup')))
+    })
+
+    test('Color is string', () => {
+      check(SuperColor, 'color', String)
+    })
+
+    test('Date is string', () => {
+      check(SuperDate, 'date', String)
+    })
+
+    test('SuperDateRange is [number, number]', () => {
+      setUp(SuperDateRange, 'dateRange')
+      const val = getValue('dateRange')
+      expect(val).toEqual(expect.any(Array))
+      expect(val).toHaveLength(2)
+      expect(val[0]).toEqual(expect.any(Number))
+      expect(val[1]).toEqual(expect.any(Number))
+    })
+
+    test('DateTime is string', () => {
+      check(SuperDateTime, 'DateTime', String)
+    })
+
+    test('SuperDateTimeRange is [number, number]', () => {
+      setUp(SuperDateTimeRange, 'SuperDateTimeRange')
+      const val = getValue('SuperDateTimeRange')
+      expect(val).toEqual(expect.any(Array))
+      expect(val).toHaveLength(2)
+      expect(val[0]).toEqual(expect.any(Number))
+      expect(val[1]).toEqual(expect.any(Number))
+    })
+
+    test('SuperMonth is string', () => {
+      check(SuperMonth, 'SuperMonth', String)
+    })
+
+    test('SuperNumber is number', () => {
+      check(SuperNumber, 'SuperNumber', Number)
+    })
+
+    test('SuperPassword is string', () => {
+      check(SuperPassword, 'SuperPassword', String)
+    })
+
+    test('SuperQuarter is string', () => {
+      check(SuperQuarter, 'SuperQuarter', String)
+    })
+
+    test('SuperRadioButton is one of options', () => {
+      const arr = ['a', 'b']
+      setUp(SuperRadioButton, 'SuperRadioButton', { options: arr })
+      expect(arr).toContain(getValue('SuperRadioButton'))
+    })
+
+    test('SuperRadioGroup is one of options', () => {
+      const arr = ['a', 'b']
+      setUp(SuperRadioGroup, 'SuperRadioGroup', { options: arr })
+      expect(arr).toContain(getValue('SuperRadioGroup'))
+    })
+
+    test('SuperRate is number', () => {
+      check(SuperRate, 'SuperRate', Number)
+      expect(getValue('SuperRate')).toBeLessThanOrEqual(5)
+      expect(getValue('SuperRate')).toBeGreaterThanOrEqual(0)
+    })
+
+    test('SuperRate set count prop', () => {
+      check(SuperRate, 'SuperRate', Number, { fieldProps: { count: 10 } })
+      expect(getValue('SuperRate')).toBeLessThanOrEqual(10)
+      expect(getValue('SuperRate')).toBeGreaterThanOrEqual(0)
+    })
+
+    test('SuperSelect is one of options', () => {
+      const arr = ['a', 'b']
+      setUp(SuperSelect, 'SuperSelect', { options: arr })
+      expect(arr).toContain(getValue('SuperSelect'))
+    })
+
+    test('when SuperSelect mode is "multiple"', () => {
+      const arr = ['a', 'b']
+      setUp(SuperSelect, 'SuperSelect', { options: arr, fieldProps: { mode: 'multiple' } })
+      expect(arr).toEqual(expect.arrayContaining(getValue('SuperSelect')))
+    })
+
+    test('when SuperSelect mode is "tags"', () => {
+      const arr = ['a', 'b']
+      setUp(SuperSelect, 'SuperSelect', { options: arr, fieldProps: { mode: 'tags' } })
+      expect(arr).toEqual(expect.arrayContaining(getValue('SuperSelect')))
+    })
+
+    test('SuperSlider is number', () => {
+      check(SuperSlider, 'SuperSlider', Number, { min: 1, max: 5 })
+      expect(getValue('SuperSlider')).toBeLessThanOrEqual(5)
+      expect(getValue('SuperSlider')).toBeGreaterThanOrEqual(1)
+    })
+
+    test('SuperSwitch is boolean', () => {
+      check(SuperSwitch, 'SuperSwitch', Boolean)
+    })
+
+    test('SuperInput is string', () => {
+      check(SuperInput, 'SuperInput', String)
+    })
+
+    test('SuperEmail is string', () => {
+      check(SuperEmail, 'SuperEmail', String)
+    })
+
+    test('SuperUrl is string', () => {
+      check(SuperUrl, 'SuperUrl', String)
+    })
+
+    test('SuperTextArea is string', () => {
+      check(SuperTextArea, 'SuperTextArea', String)
+    })
+
+    test('SuperTime is string', () => {
+      check(SuperTime, 'SuperTime', String)
+    })
+
+    test('SuperWeek is string', () => {
+      check(SuperWeek, 'SuperWeek', String)
+    })
+
+    test('SuperYear is string', () => {
+      check(SuperYear, 'SuperYear', String)
+    })
+  })
+});
