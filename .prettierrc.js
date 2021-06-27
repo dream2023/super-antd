a28aa1298516be6024063a5bcc8ee50dc408cc3f
zhangchaojie <1098626505@qq.com>
2021-06-27 21:17:04 +0800

    fix: 修复 prettier 排序问题

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,6 @@ const fabric = require('@umijs/fabric');
 
 module.exports = {
   ...fabric.prettier,
-  plugins: ['prettier-plugin-sorted', 'prettier-plugin-jsdoc', 'prettier-plugin-style-order'],
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 120,
